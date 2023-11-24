ab5b7d24569afe226853586e3bcfd80b00df0661
broken2077 <103232586+dodobird2077@users.noreply.github.com>
2023-11-24 17:19:12 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ Vue.use(AMap)
 
 AMap.initAMapApiLoader({
   // 高德的key
-  key: '7333fda4f079977bb5596730631c7075',
+  key: '',
   // 插件集合
   plugin: ['AMapManager', 'AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor','Geocoder','Geolocation', 'AMap.MarkerClusterer','AMap.PolyEditor', 'AMap.CircleEditor', 'AMap.MouseTool', 'AMap.Driving', 'AMap.CitySearch', 'AMap.InfoWindow', 'AMap.LngLat', 'AMap.DistrictSearch', 'AMap.TileLayer.Traffic', 'AMap.Heatmap', 'AMap.Autocomplete', 'AMap.PlaceSearch'],
   // 高德 sdk 版本，默认为 1.4.4
